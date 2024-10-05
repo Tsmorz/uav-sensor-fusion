d9c88c7c59842861ea342ce1fb84be87550c6d18
Tsmorz <tonysmoragiewicz@gmail.com>
2024-10-05 13:35:54 -0400

    add doc strings

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,14 @@
+"""public doc string."""
+
 import pytest
 
 
 def sample_function(variable: int) -> int:
+    """Sample function for testing.
+
+    :param variable: variable passed into function.
+    :return: variable passed in is passed out.
+    """
     return variable
 
 
@@ -9,7 +16,7 @@ def sample_function(variable: int) -> int:
     ("variable", "expected_answer"), [(1, 1), (2, 2), (3, 3), (4, 4)]
 )
 def test_sample_function(variable: int, expected_answer: int):
-    """Test sample function"""
+    """Test sample function."""
     # Arrange
 
     # Act
